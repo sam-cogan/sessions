39017a4479971fb6f59e35503b008687b981f07c
Sam Cogan <mail@samcogan.com>
2019-01-26 14:38:28 +0000

    Add mising brackets

--- PesterTesting/Demos/Test-AzureRMTemplateFile.ps1
+++ PesterTesting/Demos/Test-AzureRMTemplateFile.ps1
@@ -1,20 +1,23 @@
 param(
-    [string]$templatefolder = '.'
+    [string]$templatefolder = '.',
+    [string]$validationResourceGroup = "PesterRG",
+    [string]$validationResourceGroupRegion = "West Europe"
+
 )
 
-$templateFiles = Get-ChildItem -Path $templatefolder\* -Include "*.json" -Exclude "*.parameters.json"
+$templateFiles = Get-ChildItem -Path $templatefolder\* -Filter "*.json" -recurse -File | Where-Object -FilterScript {(Get-Content -Path $_.FullName -Raw) -ilike "*schema.management.azure.com/*/deploymentTemplate.json*"}
 
 foreach ($templateFile in $templateFiles) {
-    Describe "Testing Template: $templateFile" -Tags Template {
-
+    Describe "Testing Template Content: $templateFile" -Tags Template {
+        $templateContents = get-content $templatefile
         try {
-            $templateProperties = (get-content "$templatefile"  | ConvertFrom-Json )
+            $templateProperties = ($templateContents  | ConvertFrom-Json )
         }
         catch {
             $ErrorMessage = $_.Exception.Message
         }
 
-        Context "Template JSON Syntax" {
+        Context "Template Syntax" {
 
             It "should be less than 1 Mb" {
                 Get-Item $templateFile| Select-Object -ExpandProperty Length | Should -BeLessOrEqual 1073741824
@@ -23,6 +26,35 @@ foreach ($templateFile in $templateFiles) {
             It "Converts from JSON" {
                 $ErrorMessage | Should -BeNullOrEmpty
             }
+
+       
+        
+
+            $missingBrackets = 0
+            For ($i = 0; $i -lt $templateContents.Length; $i++) {
+                $Matches = [System.Text.RegularExpressions.Regex]::Matches($templateContents[$i], "\"".*\""")
+          
+                ForEach ($Match In $Matches) {
+                    $SquarePairCharNumber = ($Match.Value.Length - $Match.Value.Replace("[", "").Replace("]", "").Length) % 2
+                    $ParenthesisPairCharNumber = ($Match.Value.Length - $Match.Value.Replace("(", "").Replace(")", "").Length) % 2
+          
+                    if ($SquarePairCharNumber -ne 0 -or $ParenthesisPairCharNumber -ne 0) {
+                        $missingBrackets++
+            
+                        It "should have same amount of opening and closing  brackets (Line $($i + 1))" {
+                            $PairCharNumber | Should -Be 0
+                        }
+          
+                        break
+                    }
+                }
+            }
+            It "Should have no missing bracket pairs" {
+                $missingBrackets | Should -Be 0
+            }
+
+
+   
         }
 
         Context "Template Structure" {
@@ -53,6 +85,121 @@ foreach ($templateFile in $templateFiles) {
             }
 
         }
+
+        Context "Parameter and Variable Checks" {
+
+            $parametersUsage = [System.Text.RegularExpressions.RegEx]::Matches($templateContents, "parameters(\(\'\w*\'\))") | Select-Object -ExpandProperty Value -Unique
+            Context "Referenced Parameters" {
+                ForEach ($parameterUsage In $parametersUsage) {
+                    $parameterUsage = $parameterUsage.SubString($parameterUsage.IndexOf("'") + 1).Replace("')", "")
+              
+                    It "should have a parameter called $parameterUsage" {
+                        $templateProperties.parameters.$parameterUsage | Should -Not -Be $null
+                    }
+                }
+            }
+          
+            $variablesUsage = [System.Text.RegularExpressions.RegEx]::Matches($templateContents, "variables(\(\'\w*\'\))") | Select-Object -ExpandProperty Value -Unique
+            Context "Referenced Variables" {
+                ForEach ($variableUsage In $variablesUsage) {
+                    $variableUsage = $variableUsage.SubString($variableUsage.IndexOf("'") + 1).Replace("')", "")
+                
+                    It "should have a variable called $variableUsage" {
+                        $templateProperties.variables.$variableUsage | Should -Not -Be $null
+                    }
+                }
+            }
+
+        }
     }
+
+}
+
+Describe "Testing Template Validity" -Tags TestDeployment {
+
+    BeforeAll {
+        $context = $null
+        try {$context = Get-AzureRmContext} catch {throw "Not logged into Azure"}
+        New-AzureRmResourceGroup -Name $validationResourceGroup -Location $validationResourceGroupRegion -force
+    }
+
+
+    Context "Validate Template" {
+        foreach ($templateFile in $templateFiles) {
+            $templateContents = get-content $templatefile
+            try {
+                $templateProperties = ($templateContents  | ConvertFrom-Json )
+            }
+            catch {
+                $ErrorMessage = $_.Exception.Message
+            }
+           
+
+            $Parameters = $templateProperties.parameters | Get-Member | Where-Object -Property MemberType -eq -VAlue "NoteProperty" | Select-Object -ExpandProperty Name
+            $TemplateParameters = @{}
+            ForEach ($Parameter In $Parameters) {
+                if (!($templateProperties.parameters.$Parameter.defaultValue)) {
+                    if ($templateProperties.parameters.$Parameter.allowedValues) {
+                        $TemplateParameters.Add($Parameter, $templateProperties.parameters.$Parameter.allowedValues[0])
+                    }
+                    else {
+                        switch ($templateProperties.parameters.$Parameter.type) {
+                            "bool" {
+                                $TemplateParameters.Add($Parameter, $true)
+                            }
+        
+                            "int" {
+                                if ($templateProperties.parameters.$Parameter.minValue) {
+                                    $TemplateParameters.Add($Parameter, $templateProperties.parameters.$Parameter.minValue)
+                                }
+                                else {
+                                    $TemplateParameters.Add($Parameter, 1)
+                                }
+                            }
+        
+                            "string" {
+                                if ($templateProperties.parameters.$Parameter.minValue) {
+                                    $TemplateParameters.Add($Parameter, "a" * $templateProperties.parameters.$Parameter.minLength)
+                                }
+                                else {
+                                    $TemplateParameters.Add($Parameter, "dummy")
+                                }
+                            }
+        
+                            "securestring" {
+                                $TemplateParameters.Add($Parameter, (convertto-securestring "dummy" -force -AsPlainText))
+                            }
+        
+                            "array" {
+                                $TemplateParameters.Add($Parameter, @(1, 2, 3))
+                            }
+        
+                            "object" {
+                                $TemplateParameters.Add($Parameter, (New-Object -TypeName psobject -Property @{"DummyProperty" = "DummyValue"}))
+                            }
+        
+                            "secureobject" {
+                                $TemplateParameters.Add($Parameter, (New-Object -TypeName psobject -Property @{"DummyProperty" = "DummyValue"}))
+                            }
+                        }
+                    }
+                }
+            }
+
+            $output = Test-AzureRmResourceGroupDeployment -ResourceGroupName "PesterRG" -TemplateFile $templateFile.FullName @TemplateParameters
+
+            It "$templateFile should be a valid template" {
+                $output | Should -BeNullOrEmpty
+            }
+        }
+    }
+
+    AfterAll {
+        $context = $null
+        try {$context = Get-AzureRmContext} catch {throw "Not logged into Azure"}
+        Remove-AzureRMResourceGroup -Name $validationResourceGroup -force
+    }
+
+    
 
 }
