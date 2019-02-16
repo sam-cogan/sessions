2a8a66a9dc2a1688566aad392c3d88d0d92b8cb9
Sam Cogan <mail@samcogan.com>
2019-02-16 12:59:10 +0000

    Create InputBinding.cs

---
+++ AzureFunctions/InputBinding.cs
@@ -0,0 +1,18 @@
+public class TableStorage
+{
+    public class MyPoco
+    {
+        public string PartitionKey { get; set; }
+        public string RowKey { get; set; }
+        public string Text { get; set; }
+    }
+
+    [FunctionName("TableInput")]
+    public static void TableInput(
+        [QueueTrigger("table-items")] string input, 
+        [Table("MyTable", "MyPartition", "{queueTrigger}")] MyPoco poco, 
+        ILogger log)
+    {
+        log.LogInformation($"PK={poco.PartitionKey}, RK={poco.RowKey}, Text={poco.Text}");
+    }
+}
