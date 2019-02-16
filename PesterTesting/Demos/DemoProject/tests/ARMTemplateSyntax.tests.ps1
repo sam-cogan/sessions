03cb25fd82d027f6d27edb450c8812852b2c2939
Sam Cogan <mail@samcogan.com>
2019-02-16 16:08:52 +0000

    Add VSCode Test Runner

---
+++ PesterTesting/Demos/DemoProject/tests/ARMTemplateSyntax.tests.ps1
@@ -0,0 +1,114 @@
+$templateFolder=(Split-path -parent $MyInvocation.MyCommand.Path).Replace('tests','templates')
+
+
+write-host $templateFolder
+$templateFiles = Get-ChildItem -Path $templatefolder\* -Filter "*.json" -recurse -File | Where-Object -FilterScript {(Get-Content -Path $_.FullName -Raw) -ilike "*schema.management.azure.com/*/deploymentTemplate.json*"}
+
+foreach ($templateFile in $templateFiles) {
+    Describe  -Tag 'PreDeployment' "Testing Template Content: $templateFile" {
+        $templateContents = get-content $templatefile.FullName
+        try {
+            $templateProperties = ($templateContents  | ConvertFrom-Json )
+        }
+        catch {
+            $ErrorMessage = $_.Exception.Message
+        }
+
+        Context "Template Syntax" {
+
+            It "$templateFile should be less than 1 Mb" {
+                Get-Item $templateFile.FullName| Select-Object -ExpandProperty Length | Should -BeLessOrEqual 1073741824
+            }
+      
+            It "$templateFile Converts from JSON" {
+                $ErrorMessage | Should -BeNullOrEmpty
+            }
+
+       
+        
+
+            $missingBrackets = 0
+            For ($i = 0; $i -lt $templateContents.Length; $i++) {
+                $Matches = [System.Text.RegularExpressions.Regex]::Matches($templateContents[$i], "\"".*\""")
+          
+                ForEach ($Match In $Matches) {
+                    $SquarePairCharNumber = ($Match.Value.Length - $Match.Value.Replace("[", "").Replace("]", "").Length) % 2
+                    $ParenthesisPairCharNumber = ($Match.Value.Length - $Match.Value.Replace("(", "").Replace(")", "").Length) % 2
+          
+                    if ($SquarePairCharNumber -ne 0 -or $ParenthesisPairCharNumber -ne 0) {
+                        $missingBrackets++
+            
+                        It "$templateFile should have same amount of opening and closing  brackets (Line $($i + 1))" {
+                            $PairCharNumber | Should -Be 0
+                        }
+          
+                        break
+                    }
+                }
+            }
+            It "$templateFile Should have no missing bracket pairs" {
+                $missingBrackets | Should -Be 0
+            }
+
+
+   
+        }
+
+        Context "Template Structure" {
+
+            It " $templateFile should have a `$schema section" {
+                $templateProperties."`$schema" | Should -Not -BeNullOrEmpty
+            }
+      
+            It "$templateFile should have a contentVersion section" {
+                $templateProperties.contentVersion | Should -Not -BeNullOrEmpty
+            }
+      
+            It "$templateFile should have a parameters section" {
+                $templateProperties.parameters | Should -Not -BeNullOrEmpty
+            }
+          
+          
+            It "$templateFile should have a variables section" {
+                $templateProperties."variables" | Should -Not -BeNullOrEmpty
+            }
+
+          
+            It "$templateFile should have a outputs section" {
+                $templateProperties."outputs" | Should -Not -BeNullOrEmpty
+            }
+            It "$templateFile should have less than 256 parameters" {
+                $templateProperties.parameters.Length | Should -BeLessOrEqual 256
+            }
+
+        }
+
+        Context "Parameter and Variable Checks" {
+
+            $parametersUsage = [System.Text.RegularExpressions.RegEx]::Matches($templateContents, "parameters(\(\'\w*\'\))") | Select-Object -ExpandProperty Value -Unique
+            Context "Referenced Parameters" {
+                ForEach ($parameterUsage In $parametersUsage) {
+                    $parameterUsage = $parameterUsage.SubString($parameterUsage.IndexOf("'") + 1).Replace("')", "")
+              
+                    It "$templateFile should have a parameter called $parameterUsage" {
+                        $templateProperties.parameters.$parameterUsage | Should -Not -Be $null
+                    }
+                }
+            }
+          
+            $variablesUsage = [System.Text.RegularExpressions.RegEx]::Matches($templateContents, "variables(\(\'\w*\'\))") | Select-Object -ExpandProperty Value -Unique
+            Context "Referenced Variables" {
+                ForEach ($variableUsage In $variablesUsage) {
+                    $variableUsage = $variableUsage.SubString($variableUsage.IndexOf("'") + 1).Replace("')", "")
+                
+                    It "should have a variable called $variableUsage" {
+                        $templateProperties.variables.$variableUsage | Should -Not -Be $null
+                    }
+                }
+            }
+
+        }
+    }
+
+}
+
